0eb856cf6017df9eaca0c6ea84f49446cc8fceff
Richart Smith Escobedo Quispe <rescobedoq@gmail.com>
2023-07-05 10:07:50 -0500

    so

---
+++ qt/hellotr/hellotr_la.ts
@@ -0,0 +1,9 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Hello world!</source>
+        <translation>¡Hola mundo!</translation>
+    </message>
+</context>
+</TS>
---
+++ qt/project/fake.pro
@@ -0,0 +1,4 @@
+TARGET = ApplicationName
+SOURCES += main.cpp Widget1.cpp Widget2.cpp
+HEADERS += Widget1.h Widget2.h
+QT += widgets
